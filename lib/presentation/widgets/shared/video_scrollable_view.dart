828f057c08a7be8f1f66f8986757b36e8981ee83
Francisco Javier García Muñoz <javidesoft@gmail.com>
2023-09-08 18:38:20 +0200

    Scrollable page

---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,27 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+import '../../../domain/entities/video_post.dart';
+
+
+class VideoScrollableView extends StatelessWidget {
+
+final List<VideoPost> videos;
+
+  const VideoScrollableView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      physics: const BouncingScrollPhysics(),
+      children: [
+        Container(color: Colors.red,),
+        Container(color: Colors.blue,),
+        Container(color: Colors.pink,),
+      ],
+    );
+  }
+
+}
